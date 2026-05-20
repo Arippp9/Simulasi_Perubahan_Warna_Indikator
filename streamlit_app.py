5135645ab029cd61380327626229ebb8f57e2c2e
UdinHeker <121849343+Arippp9@users.noreply.github.com>
2026-05-20 20:26:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,57 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# data larutan
+data = {
+    "HCl": {
+        "pH": 1,
+        "sifat": "Asam",
+        "warna": "Tidak berwarna"
+    },
+
+    "NaOH": {
+        "pH": 13,
+        "sifat": "Basa",
+        "warna": "Pink"
+    },
+
+    "CH3COOH": {
+        "pH": 3,
+        "sifat": "Asam",
+        "warna": "Tidak berwarna"
+    },
+
+    "NH4OH": {
+        "pH": 11,
+        "sifat": "Basa",
+        "warna": "Pink"
+    }
+}
+
+# title
+st.title("🧪 ChemIndicator")
+st.write("Simulasi indikator asam basa")
+
+# pilih larutan
+larutan = st.selectbox(
+    "Pilih Larutan",
+    list(data.keys())
 )
+
+# tombol cek
+if st.button("Cek Hasil"):
+
+    hasil = data[larutan]
+
+    st.subheader("Hasil Simulasi")
+
+    st.write(f"**Larutan :** {larutan}")
+    st.write(f"**Sifat :** {hasil['sifat']}")
+    st.write(f"**pH :** {hasil['pH']}")
+    st.write(f"**Warna indikator :** {hasil['warna']}")
+
+    # warna visual
+    if hasil["sifat"] == "Asam":
+        st.error("🔴 Larutan Bersifat Asam")
+
+    else:
+        st.success("🔵 Larutan Bersifat Basa")
